f9e04274aba03c6751c7e2b9e59aee2892803d1a
chrisuth <chrisuth@stud.ntnu.no>
2024-03-19 06:00:08 +0100

    Fix: verifing hovedscene and Add: args to run main

--- src/python/main.py
+++ src/python/main.py
@@ -1,14 +1,28 @@
 import sqlite3
 import os
+import sys
 
 from verifyDB import verifyDB
-from populateDB import populateDB
+from verifyScenes import verifyScenes
 
 database = os.path.join("src", "sql", "database.db")
 
 conn = sqlite3.connect(database)
 cursor = conn.cursor()
 
+def main():
+    if len(sys.argv) > 1:
+        print(sys.argv[1])
+        if sys.argv[1] == "verify":
+            verifyDB(conn)
+            verifyScenes(conn)
+            conn.commit()
+            conn.close()
+        else:
+            print("Invalid arguments")
+    else:
+        print("No arguments was given. Please provide an argument.")
+        print("The following arguments are available:")
+        print(" - verify: Verify the database and populate it with some data.")
 if __name__ == "__main__":
-    verifyDB(conn)
-    populateDB(conn)
+    main()
--- src/python/verifyScenes.py
+++ src/python/verifyScenes.py
@@ -1,7 +1,9 @@
 from models import Sal, Område, Rad, Stol
 import re
 
-def populateHovedsal(cursor):
+def verifyHovedscene(cursor):
+    '''Verify the database with the hovedsal'''
+    print("Verifying hovedscene...")
     hovedscene = Sal("hovedscene")
     cursor.execute(hovedscene.get())
     if cursor.fetchone() is None:
@@ -19,16 +21,15 @@ def populateHovedsal(cursor):
     all_områder_db = cursor.execute(Område.get_all()).fetchall()
     all_rad_db = cursor.execute(Rad.get_all()).fetchall()
     all_stol_db = cursor.execute(Stol.get_all()).fetchall()
+    print(all_stol_db)
 
     all_områder = []
-    print("Getting all områder")
+    print("Getting all områder...")
     for område in all_områder_db:
         gameltOmråde = Område(område[0], område[1], område[2])
         all_områder.append(gameltOmråde)
 
-    for område in all_områder:
-        print(område.__str__())
-
+    print("Verifying områder...")
     for row in content:
         hasDigit = bool(re.search(r'\d', row))
         if not hasDigit and row.strip() != "":
@@ -40,7 +41,6 @@ def populateHovedsal(cursor):
                     exist = True
                     break
             if not exist:
-                print(current_område.insert())
                 cursor.execute(current_område.insert())
                 all_områder.append(current_område)
             område_counter += 1
@@ -49,47 +49,63 @@ def populateHovedsal(cursor):
     område_counter = 0
     current_område = all_områder[område_counter]
     all_rad = []
-    print("Getting all rad")
+    print("Getting all rad...")
     for rad in all_rad_db:
-        gamelRad = Rad(rad[0], rad[1], rad[2])
-        all_rad.append(gamelRad)
+        for område in all_områder:
+            if rad[2] == område.navn:
+                gamelRad = Rad(rad[0], rad[1], område)
+                all_rad.append(gamelRad)
+                break
     
     all_stol = []
-    print("Getting all stol")
+    print("Getting all stol...")
     for stol in all_stol_db:
-        gamelStol = Stol(stol[0], stol[1], stol[2])
-        all_stol.append(gamelStol)
-
+        for rad in all_rad:
+            if stol[2] == rad.id:
+                gamelStol = Stol(stol[0], stol[1], rad)
+                all_stol.append(gamelStol)
+                print(gamelStol)
+                break
+    
+    print("Verifying rad and stol...")
     for row in content:
         row = row.strip()
-        print("Row: ", rad_counter)
         hasDigit = bool(re.search(r'\d', row))
-        print("Has digit: ", hasDigit)
         if not hasDigit and row.strip() != "":
             current_område = all_områder[område_counter]
             område_counter += 1
         elif hasDigit and row.strip() != "":
-            current_rad = Rad(rad_counter, rad_counter + 1, current_område.navn)
-            if current_rad not in all_rad:
-                print(current_rad.insert())
+            current_rad = Rad(rad_counter, rad_counter + 1, current_område)
+            exist = False
+            for rad in all_rad:
+                if current_rad.radnr == rad.radnr and current_rad.område.navn == rad.område.navn and current_rad.id == rad.id:
+                    current_rad = rad
+                    exist = True
+                    break
+            if not exist:
                 cursor.execute(current_rad.insert())
                 all_rad.append(current_rad)
             rad_counter += 1
             for i in range(1, len(row.strip()) + 1):
-                print(i)
                 if row[i - 1] == "x":
                     stol_counter += 1
                     continue
                 current_stol = Stol(stol_counter, stol_counter + 1, current_rad)
-                if current_stol not in all_stol:
-                    print(current_stol.insert())
+                exist = False
+                for stol in all_stol:
+                    if current_stol.stolnr == stol.stolnr and current_stol.rad.radnr == stol.rad.radnr and current_stol.rad.område.navn == stol.rad.område.navn and current_stol.id == stol.id:
+                        current_stol = stol
+                        exist = True
+                        break
+                if not exist:
+                    print(stol)
                     cursor.execute(current_stol.insert())
                     all_stol.append(current_stol)
                 stol_counter += 1
 
 
-def populateDB(conn):
+def verifyScenes(conn):
     '''Populate the database with some data'''
     cursor = conn.cursor()
-    populateHovedsal(cursor)
+    verifyHovedscene(cursor)
     conn.commit()
