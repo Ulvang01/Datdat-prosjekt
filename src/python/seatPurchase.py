ec5b41272155d1f18fdef216fcdc24faf81a696d
Idar Erlend Bendiksen Buer <iebuer@stud.ntnu.no>
2024-03-21 11:00:39 +0100

    Finished get free seats function

--- src/python/seatPurchase.py
+++ src/python/seatPurchase.py
@@ -11,25 +11,39 @@ def concatinateStingList(stringList: List[str]) -> str:
 
 def getFreeSeats(cursor: sqlite3.Cursor, play: List[str], date: str) -> None:
     date = datetime.date(int(date[:4]), int(date[5:7]), int(date[8:]))
-    print(play, type(date))
     nameOfPlay = concatinateStingList(play)
-    print(nameOfPlay)
     
     query = '''
-    SELECT Stol.rad, COUNT(Stol.id) FROM Stol JOIN Billett ON Stol.id = Billett.stol 
-	    JOIN Visning ON Billett.visning = Visning.id 
-            JOIN Teaterstykket ON Visning.teaterstykket = Teaterstykket.id
-		        WHERE Visning.dato = ? AND Teaterstykket.navn = ?
-        GROUP BY Stol.rad
+    SELECT Rad.id, COUNT(Stol.id) FROM Stol JOIN Billett ON Stol.id = Billett.stol 
+        RIGHT OUTER JOIN Rad ON Stol.rad = Rad.id
+            JOIN Visning ON Billett.visning = Visning.id 
+                JOIN Teaterstykket ON Visning.teaterstykket = Teaterstykket.id
+                    WHERE Visning.dato = ? AND Teaterstykket.navn = ?
+        GROUP BY Rad.id
     '''
     tickets = cursor.execute(query, (date, nameOfPlay)).fetchall()
-    print(tickets)
-    print(len(tickets))
+    
     
     query = '''
-    SELECT Rad.id, Rad.radnr, Område.navn FROM Rad JOIN Område ON Rad.område = Område.id
-        WHERE Område.sal IN (SELECT Teaterstykket.sal FROM Teaterstykket
-            WHERE Teaterstykket.navn = ?)
+    SELECT Rad.id, Rad.radnr, Område.navn, COUNT(Stol.id) FROM Rad JOIN Område ON Rad.område = Område.id
+        JOIN Stol ON Stol.rad = Rad.id
+            WHERE Område.sal IN (SELECT Teaterstykket.sal FROM Teaterstykket
+                WHERE Teaterstykket.navn = ?)
+        GROUP BY Rad.id
+        ORDER BY Område.navn, Rad.radnr DESC
     '''
     rows = cursor.execute(query, (nameOfPlay,)).fetchall()
-    print(rows)
+    
+    prevArea = ""
+    for row in rows:
+        if row[2] != prevArea:
+            print(row[2])
+            prevArea = row[2]
+        
+        ticketsTaken = 0
+        for ticket in tickets:
+            if ticket[0] == row[0]:
+                ticketsTaken = ticket[1]
+                break
+        freeSeats = row[3] - ticketsTaken
+        print(f"Row: {row[1]} Free Seats: {freeSeats}")
