de2bffac9db4da72104566be1ca7c0994995852d
Idar Erlend Bendiksen Buer <iebuer@stud.ntnu.no>
2024-03-21 19:05:29 +0100

    Fixed merge problems

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
         inp = input()
         if inp == 'x':
             break
-        if inp == 'help':
+        elif inp == 'help':
             print("\nThe following arguments are available:")
             print(" - verify  --> Verify the database and populate it with some data.")
             print(" - getActorsByPlay <name of play>  --> Get all actors in a given play.")
@@ -46,8 +46,12 @@ def main():
                 continue
             actors = Skuespiller.get_all_by_play(cursor, play.id)
             for actor in actors: 
-                print(actor)
-        if inp.split(' ')[0] == 'getActorConnections':
+                print(actor.__str__())
+        if inp.split(' ')[0] == 'getBestsellingScreening':
+            best_play = Visning.get_bestselling(cursor)
+            print("Best selling screening is: ", best_play[0].teaterstykket.navn, " at ", best_play[0].dato, ".")
+            print("And it has sold: ", best_play[1], " tickets.")
+        elif inp.split(' ')[0] == 'getActorConnections':
             acts = Akt.get_acts_by_actor(cursor, inp.split(' ', 1)[1])
             for act in acts:
                 connection = Skuespiller.get_actors_by_act(cursor, act.id)
@@ -64,11 +68,11 @@ def main():
             for play in plays:
                 count = Billett.get_amount_by_play_and_date(cursor, play.id, inp.split(' ')[1])
                 print(play, f'(Solgte_billetter={count})')
-        if inp.split(' ')[0] == 'getBestsellingScreening':
+        elif inp.split(' ')[0] == 'getBestsellingScreening':
             best_play = Visning.get_bestselling(cursor)
             print("Best selling screening is: ", best_play[0].teaterstykket.navn, " at ", best_play[0].dato, ".")
             print("And it has sold: ", best_play[1], " tickets.")
-        if inp.split(' ')[0] == 'makeCustomerProfile':
+        elif inp.split(' ')[0] == 'makeCustomerProfile':
             inp = inp[inp.find(' ')+1:]
             argumentList = [element.strip() for element in inp.split(',')]
             if len(argumentList) != 3:
@@ -76,14 +80,14 @@ def main():
             else:
                 makeCustomerProfile(cursor, *argumentList)
                 conn.commit()
-        if inp.split(' ')[0] == 'getFreeSeats':
+        elif inp.split(' ')[0] == 'getFreeSeats':
             inp = inp[inp.find(' ')+1:]
             argumentList = [element.strip() for element in inp.split(',')]
             if len(argumentList) != 2:
                 print("Invalid number of arguments")
             else:
                 getFreeSeats(cursor, *argumentList)
-        if inp.split(' ')[0] == 'purchaseTickets':
+        elif inp.split(' ')[0] == 'purchaseTickets':
             inp = inp[inp.find(' ')+1:]
             argumentList = [element.strip() for element in inp.split(',')]
             if len(argumentList) != 7:
@@ -91,6 +95,9 @@ def main():
             else:
                 purchaseTickets(cursor, *argumentList)
                 conn.commit()
+        else:
+            print('Unrecognized command, type "help" for a list of all commands')
+        
 
     conn.close()
 if __name__ == "__main__":
